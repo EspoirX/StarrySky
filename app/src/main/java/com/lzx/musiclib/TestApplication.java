ad741cd8df9687cf9a40ec5d0c1133c68f435d65
lizixian <386707112@qq.com>
2019-01-26 22:36:05 +0800

    update readme

--- app/src/main/java/com/lzx/musiclib/TestApplication.java
+++ app/src/main/java/com/lzx/musiclib/TestApplication.java
@@ -1,6 +1,8 @@
 package com.lzx.musiclib;
 
 import android.app.Application;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Environment;
 
 import com.lzx.starrysky.manager.MusicManager;
@@ -14,6 +16,18 @@ import com.lzx.starrysky.playback.download.ExoDownload;
  */
 public class TestApplication extends Application {
 
+    public static String ACTION_PLAY_OR_PAUSE = "ACTION_PLAY_OR_PAUSE";
+    public static  String ACTION_NEXT = "ACTION_NEXT";
+    public static  String ACTION_PRE = "ACTION_PRE";
+    public static  String ACTION_FAVORITE = "ACTION_FAVORITE";
+    public static  String ACTION_LYRICS = "ACTION_LYRICS";
+
+    private PendingIntent getPendingIntent(String action) {
+        Intent intent = new Intent(action);
+        intent.setClass(this, NotificationReceiver.class);
+        return PendingIntent.getBroadcast(this, 0, intent, 0);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -22,8 +36,15 @@ public class TestApplication extends Application {
         //配置通知栏
         NotificationConstructor constructor = new NotificationConstructor.Builder()
                 .setCreateSystemNotification(false)
+                .setPlayOrPauseIntent(getPendingIntent(ACTION_PLAY_OR_PAUSE))
+                .setNextIntent(getPendingIntent(ACTION_NEXT))
+                .setPreIntent(getPendingIntent(ACTION_PRE))
+                .setFavoriteIntent(getPendingIntent(ACTION_FAVORITE))
+                .setLyricsIntent(getPendingIntent(ACTION_LYRICS))
                 .bulid();
+
         MusicManager.getInstance().setNotificationConstructor(constructor);
+
         //设置缓存
         String destFileDir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/11ExoCacheDir";
         ExoDownload.getInstance().setOpenCache(true); //打开缓存开关
