bc13358b80c3cadc2d988e1eb37eef67cac7f4df
lizixian <386707112@qq.com>
2019-09-03 17:11:29 +0800

    update readme

--- app/src/main/java/com/lzx/musiclib/MusicModule.java
+++ app/src/main/java/com/lzx/musiclib/MusicModule.java
@@ -3,16 +3,14 @@ package com.lzx.musiclib;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import com.lzx.musiclib.imageloader.GlideLoader;
 import com.lzx.starrysky.StarrySkyBuilder;
-import com.lzx.starrysky.registry.StarrySkyRegistry;
 import com.lzx.starrysky.StarrySkyConfig;
+import com.lzx.starrysky.registry.StarrySkyRegistry;
 
 public class MusicModule extends StarrySkyConfig {
     @Override
     public void applyOptions(@NonNull Context context, @NonNull StarrySkyBuilder builder) {
         super.applyOptions(context, builder);
-
     }
 
     @Override
