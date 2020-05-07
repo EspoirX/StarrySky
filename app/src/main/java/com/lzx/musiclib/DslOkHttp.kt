f7ae11be111604374a289af14853f5c720bedf41
lizixian <lizixian@yy.com>
2020-05-07 19:07:21 +0800

    修改一下demo的请求方式

---
+++ app/src/main/java/com/lzx/musiclib/DslOkHttp.kt
@@ -0,0 +1,84 @@
+package com.lzx.musiclib
+
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+
+class DslOkHttp(var async: Boolean) {
+
+    private val client: OkHttpClient = OkHttpClient().newBuilder()
+        .addInterceptor(object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val newRequest = chain.request().newBuilder()
+                    .removeHeader("User-Agent")
+                    .addHeader("User-Agent",
+                        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:0.9.4)")
+                    .build()
+                return chain.proceed(newRequest)
+            }
+        }).build()
+
+    private var url: String = ""
+    private var callSuccess: ((String) -> Unit)? = null
+    private var callFailure: ((e: Throwable?, errorMsg: String) -> Unit)? = null
+
+    fun url(init: DslOkHttp.() -> String) {
+        url = init()
+        doRequest()
+    }
+
+    fun onSuccess(onSuccess: (String) -> Unit) {
+        callSuccess = onSuccess
+    }
+
+    fun onFailure(onFailure: (e: Throwable?, errorMsg: String) -> Unit) {
+        callFailure = onFailure
+    }
+
+    fun doRequestSync(): String {
+        var json = ""
+        val request = Request.Builder().url(url).build()
+        try {
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                json = response.body!!.string()
+                callSuccess?.invoke(json)
+            } else {
+                callFailure?.invoke(null, "response is Fail")
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            callFailure?.invoke(ex, ex.message.toString())
+        }
+        return json
+    }
+
+    private fun doRequest() {
+        val request = Request.Builder().url(url).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callFailure?.invoke(e, e.message.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                callSuccess?.invoke(response.body!!.string())
+            }
+        })
+    }
+}
+
+fun dslOkHttpAsync(init: DslOkHttp.() -> Unit): DslOkHttp {
+    val http = DslOkHttp(true)
+    http.init()
+    return http
+}
+
+fun dslOkHttpSync(init: DslOkHttp.() -> Unit): String {
+    val http = DslOkHttp(false)
+    http.init()
+    return http.doRequestSync()
+}
--- app/src/main/java/com/lzx/musiclib/example/MusicRequest.kt
+++ app/src/main/java/com/lzx/musiclib/example/MusicRequest.kt
@@ -1,7 +1,9 @@
 package com.lzx.musiclib.example
 
+import android.util.Log
 import android.widget.Toast
 import com.lzx.musiclib.TestApplication
+import com.lzx.musiclib.dslOkHttpSync
 import com.lzx.starrysky.provider.SongInfo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,29 +11,9 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
 import org.json.JSONObject
-import kotlin.time.days
 
 open class MusicRequest : CoroutineScope by MainScope() {
-    private val client: OkHttpClient
-
-    init {
-        client = OkHttpClient().newBuilder()
-            .addInterceptor(object : Interceptor {
-                override fun intercept(chain: Interceptor.Chain): Response {
-                    val newRequest = chain.request().newBuilder()
-                        .removeHeader("User-Agent")
-                        .addHeader("User-Agent",
-                            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:0.9.4)")
-                        .build()
-                    return chain.proceed(newRequest)
-                }
-            }).build()
-    }
 
     data class MediaInfo(
         val songid: String, val songmid: String, val songname: String, val singer: String
@@ -39,67 +21,60 @@ open class MusicRequest : CoroutineScope by MainScope() {
 
     private fun getSongList(): MutableList<MediaInfo> {
         val list = mutableListOf<MediaInfo>()
-        try {
-            val url = "https://api.qq.jsososo.com/songlist?id=7382629476"
-            val request = Request.Builder().url(url).build()
-            val response = client.newCall(request).execute()
-            if (response.isSuccessful) {
-                val json = response.body!!.string()
-                val jsonObject = JSONObject(json)
-                val jsonArray = jsonObject.getJSONObject("data").getJSONArray("songlist")
-                for (i in 0 until jsonArray.length()) {
-                    val obj = jsonArray.getJSONObject(i)
-                    val singer = obj.getJSONArray("singer")
-                    val artist = if (singer.length() > 0) {
-                        singer.getJSONObject(0).getString("name")
-                    } else {
-                        "未知歌手"
-                    }
-                    val media = MediaInfo(
-                        obj.getString("songid"),
-                        obj.getString("songmid"),
-                        obj.getString("songname"),
-                        artist
-                    )
-                    list.add(media)
-                }
+        val json = dslOkHttpSync {
+            url { "https://api.qq.jsososo.com/songlist?id=7382629476" }
+            onFailure { e, errorMsg ->
+                Log.i("MusicRequest", "onFailure = $errorMsg")
             }
-        } catch (ex: Exception) {
-            ex.printStackTrace()
+        }
+        val jsonArray = JSONObject(json).getJSONObject("data").getJSONArray("songlist")
+        for (i in 0 until jsonArray.length()) {
+            val obj = jsonArray.getJSONObject(i)
+            val singer = obj.getJSONArray("singer")
+            val artist = if (singer.length() > 0) {
+                singer.getJSONObject(0).getString("name")
+            } else {
+                "未知歌手"
+            }
+            val media = MediaInfo(
+                obj.getString("songid"),
+                obj.getString("songmid"),
+                obj.getString("songname"),
+                artist
+            )
+            list.add(media)
         }
         return list
     }
 
     private fun getSongDetail(list: MutableList<MediaInfo>): MutableList<SongInfo> {
         val songList = mutableListOf<SongInfo>()
-        try {
-            val stringBuilder = StringBuilder()
-            list.forEach {
-                stringBuilder.append(it.songmid).append(",")
-            }
-            val result = stringBuilder.toString();
-            val ids = result.substring(0, result.length - 1)
-            val url = "https://api.qq.jsososo.com/song/batch?songmids=$ids"
-            val request = Request.Builder().url(url).build()
-            val response = client.newCall(request).execute()
-            if (response.isSuccessful) {
-                val json = response.body!!.string()
-                val jsonObject = JSONObject(json)
+        val json = dslOkHttpSync {
+            url {
+                val stringBuilder = StringBuilder()
                 list.forEach {
-                    val songInfo = SongInfo()
-                    val data = jsonObject.getJSONObject("data").getJSONObject(it.songmid)
-                    val mid =
-                        data.getJSONObject("track_info").getJSONObject("album").getString("mid")
-                    val songCover = "https://y.gtimg.cn/music/photo_new/T002R300x300M000${mid}.jpg"
-                    songInfo.songId = it.songmid
-                    songInfo.songName = it.songname
-                    songInfo.artist = it.singer
-                    songInfo.songCover = songCover
-                    songList.add(songInfo)
+                    stringBuilder.append(it.songmid).append(",")
                 }
+                val result = stringBuilder.toString();
+                val ids = result.substring(0, result.length - 1)
+                return@url "https://api.qq.jsososo.com/song/batch?songmids=$ids"
             }
-        } catch (ex: java.lang.Exception) {
-            ex.printStackTrace()
+            onFailure { e, errorMsg ->
+                Log.i("MusicRequest", "onFailure = $errorMsg")
+            }
+        }
+        val jsonObject = JSONObject(json)
+        list.forEach {
+            val songInfo = SongInfo()
+            val data = jsonObject.getJSONObject("data").getJSONObject(it.songmid)
+            val mid =
+                data.getJSONObject("track_info").getJSONObject("album").getString("mid")
+            val songCover = "https://y.gtimg.cn/music/photo_new/T002R300x300M000${mid}.jpg"
+            songInfo.songId = it.songmid
+            songInfo.songName = it.songname
+            songInfo.artist = it.singer
+            songInfo.songCover = songCover
+            songList.add(songInfo)
         }
         return songList
     }
@@ -120,23 +95,17 @@ open class MusicRequest : CoroutineScope by MainScope() {
 
     fun requestSongUrl(songmid: String, callback: RequestInfoCallback) {
         launch(Dispatchers.IO) {
-            try {
-                val url = "https://api.qq.jsososo.com/song/urls?id=$songmid"
-                val request = Request.Builder().url(url).build()
-                val response = client.newCall(request).execute()
-                if (response.isSuccessful) {
-                    val json = response.body!!.string()
-                    val jsonObject = JSONObject(json)
-                    val url = jsonObject.getJSONObject("data").getString(songmid)
-                    withContext(Dispatchers.Main) {
-                        callback.onSuccess(url)
-                    }
+            val json = dslOkHttpSync {
+                url { "https://api.qq.jsososo.com/song/urls?id=$songmid" }
+                onFailure { e, errorMsg ->
+                    Log.i("MusicRequest", "onFailure = $errorMsg")
                 }
-            } catch (ex: java.lang.Exception) {
-                ex.printStackTrace()
+            }
+            val url = JSONObject(json).getJSONObject("data").getString(songmid)
+            withContext(Dispatchers.Main) {
+                callback.onSuccess(url)
             }
         }
-
     }
 
     interface RequestCallback {
