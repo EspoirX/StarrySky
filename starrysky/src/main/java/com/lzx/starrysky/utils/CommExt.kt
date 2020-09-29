214f962333f8ffcd61415316d75da7d739c9a16d
lizixian <lizixian@yy.com>
2020-09-29 17:57:18 +0800

    代码微调

--- starrysky/src/main/java/com/lzx/starrysky/utils/CommExt.kt
+++ starrysky/src/main/java/com/lzx/starrysky/utils/CommExt.kt
@@ -1,35 +1,19 @@
 package com.lzx.starrysky.utils
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.ActivityManager
 import android.app.ActivityManager.RunningAppProcessInfo
+import android.app.Application
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Context.ACTIVITY_SERVICE
 import android.content.Intent
+import android.os.Build
 import android.os.Process
 import android.widget.Toast
 import com.lzx.starrysky.notification.INotification
 import java.util.Locale
-
-fun <T> Int.isIndexPlayable(queue: List<T>?): Boolean {
-    return queue != null && this >= 0 && this < queue.size
-}
-
-
-/**
- * 得到目标界面 Class
- */
-fun String?.getTargetClass(): Class<*>? {
-    var clazz: Class<*>? = null
-    try {
-        if (!this.isNullOrEmpty()) {
-            clazz = Class.forName(this)
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
-    return clazz
-}
 
 fun Context.getResourceId(name: String, className: String): Int {
     val packageName = applicationContext.packageName
@@ -64,6 +48,70 @@ fun Context.isMainProcess(): Boolean {
         } while (info.pid != Process.myPid())
         this.packageName == info.processName
     }
+}
+
+fun Context.isPatchProcess(): Boolean {
+    val am = this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    val runningApp = am.runningAppProcesses
+    return if (runningApp == null) {
+        false
+    } else {
+        val var3: Iterator<*> = runningApp.iterator()
+        var info: RunningAppProcessInfo
+        do {
+            if (!var3.hasNext()) {
+                return false
+            }
+            info = var3.next() as RunningAppProcessInfo
+        } while (info.pid != Process.myPid())
+        info.processName.endsWith("patch")
+    }
+}
+
+/**
+ * 判断Activity 是否可用
+ */
+fun Context?.isActivityAvailable(): Boolean {
+    if (null == this) return false
+    if (this !is Activity) return false
+    if (this.isFinishing) return false
+    return !(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && this.isDestroyed)
+}
+
+
+/**
+ * 反射一下主线程获取一下上下文
+ */
+val contextReflex: Application?
+    get() = try {
+        @SuppressLint("PrivateApi")
+        val activityThreadClass = Class.forName("android.app.ActivityThread")
+        @SuppressLint("DiscouragedPrivateApi")
+        val currentApplicationMethod = activityThreadClass.getDeclaredMethod("currentApplication")
+        currentApplicationMethod.isAccessible = true
+        currentApplicationMethod.invoke(null) as Application
+    } catch (ex: Exception) {
+        ex.printStackTrace()
+        null
+    }
+
+fun <T> Int.isIndexPlayable(queue: List<T>?): Boolean {
+    return queue != null && this >= 0 && this < queue.size
+}
+
+/**
+ * 得到目标界面 Class
+ */
+fun String?.getTargetClass(): Class<*>? {
+    var clazz: Class<*>? = null
+    try {
+        if (!this.isNullOrEmpty()) {
+            clazz = Class.forName(this)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    return clazz
 }
 
 fun String.md5(): String {
--- starrysky/src/main/java/com/lzx/starrysky/utils/StarrySkyUtils.kt
+++ starrysky/src/main/java/com/lzx/starrysky/utils/StarrySkyUtils.kt
@@ -1,69 +1,12 @@
 package com.lzx.starrysky.utils
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.app.ActivityManager
-import android.app.Application
-import android.content.Context
 import android.os.Build
-import android.os.Process
 import android.util.Log
 import com.lzx.starrysky.control.RepeatMode
 import org.json.JSONObject
 
 object StarrySkyUtils {
     var isDebug = true
-
-    /**
-     * 判断Activity 是否可用
-     */
-    fun isActivityAvailable(activity: Activity?): Boolean {
-        if (null == activity) {
-            return false
-        }
-        if (activity.isFinishing) {
-            return false
-        }
-        return !(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && activity.isDestroyed)
-    }
-
-    fun isPatchProcess(context: Context): Boolean {
-        val am =
-            context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val runningApp =
-            am.runningAppProcesses
-        return if (runningApp == null) {
-            false
-        } else {
-            val var3: Iterator<*> = runningApp.iterator()
-            var info: ActivityManager.RunningAppProcessInfo
-            do {
-                if (!var3.hasNext()) {
-                    return false
-                }
-                info = var3.next() as ActivityManager.RunningAppProcessInfo
-            } while (info.pid != Process.myPid())
-            info.processName.endsWith("patch")
-        }
-    }
-
-    /**
-     * 反射一下主线程获取一下上下文
-     */
-    val contextReflex: Application?
-        get() = try {
-            @SuppressLint("PrivateApi") val activityThreadClass =
-                Class.forName("android.app.ActivityThread")
-
-            @SuppressLint("DiscouragedPrivateApi")
-            val currentApplicationMethod =
-                activityThreadClass.getDeclaredMethod("currentApplication")
-            currentApplicationMethod.isAccessible = true
-            currentApplicationMethod.invoke(null) as Application
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-            null
-        }
 
     fun saveRepeatMode(repeatMode: Int, isLoop: Boolean) {
         try {
